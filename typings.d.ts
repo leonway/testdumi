d5013d40ee2719c5aac31a76b78b3fbc9a781442
liming.cao <liming.cao@rokid.com>
2021-08-03 12:01:20 +0800

    update

---
+++ typings.d.ts
@@ -0,0 +1,8 @@
+declare module '*.css';
+declare module '*.less';
+declare module '*.svg' {
+  const content: React.FunctionComponent<React.SVGAttributes<SVGElement>>;
+  export default content;
+}
+declare module '*.png';
+declare module '@sky/media-preview';
